5220329cbad2719a7a94b264bd699866773d4274
Bruno Tarditi <brunotarditi@hotmail.com>
2021-04-16 19:46:50 -0300

    Creo el proyecto spring boot para el backend

---
+++ Api-Buen-Sabor/src/main/java/com/example/buensabor/ApiBuenSaborApplication.java
@@ -0,0 +1,13 @@
+package com.example.buensabor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiBuenSaborApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiBuenSaborApplication.class, args);
+	}
+
+}
