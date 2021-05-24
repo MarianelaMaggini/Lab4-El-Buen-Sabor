14b84a2c28ae37c356facb379bd1f6c5d5966a2b
Bruno Tarditi <brunotarditi@hotmail.com>
2021-05-24 13:47:29 -0300

    Subo service y controlador para cargar archivos y obtenerlos

---
+++ Api-Buen-Sabor/src/main/java/com/example/buensabor/entities/archivos/Archivo.java
@@ -0,0 +1,13 @@
+package com.example.buensabor.entities.archivos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Archivo {
+    private String name;
+    private String url;
+}
