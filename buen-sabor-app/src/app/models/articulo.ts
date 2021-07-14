a1f0772bf1f36b880ddb897771dcc640d288825a
Bruno Tarditi <brunotarditi@hotmail.com>
2021-07-14 00:22:56 -0300

    Añadiendo eliminaciones lógicas

--- buen-sabor-app/src/app/models/articulo.ts
+++ buen-sabor-app/src/app/models/articulo.ts
@@ -1,9 +1,13 @@
 import { Rubro } from "./rubro";
+import { TipoArticulo } from "./tipo-articulo";
+import { UnidadMedida } from "./unidad-medida";
 
 export interface Articulo {
     id: number;
     denominacion: string;
     imagen: string;
     precioVenta: number;
+    tipoArticulo: TipoArticulo;
     rubro: Rubro;
+    unidadMedida: UnidadMedida;
 }
--- buen-sabor-app/src/app/models/pedido.ts
+++ buen-sabor-app/src/app/models/pedido.ts
@@ -1,2 +1,12 @@
+import { Domicilio } from "./domicilio";
+import { TipoEnvio } from "./tipo-envio";
+import { Usuario } from "./usuario";
+
 export class Pedido {
+    numeroPedido: number;
+    horaEstimadaFin: Date;
+    total: number;
+    usuario: Usuario;
+    tipoEnvio: TipoEnvio;
+    domicilio: Domicilio;
 }
