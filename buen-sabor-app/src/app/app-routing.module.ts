861072bbb0fc0c38cb29063d48f71519139a3e2c
Bruno Tarditi <brunotarditi@hotmail.com>
2021-10-11 18:31:38 -0300

    Arreglando app routing module

--- buen-sabor-app/src/app/app-routing.module.ts
+++ buen-sabor-app/src/app/app-routing.module.ts
@@ -1,27 +1,12 @@
 import { NgModule } from '@angular/core';
-
-// Rutas
 import { RouterModule, Routes } from '@angular/router';
 
-// Inicio
-import { HomeComponent } from './modules/articulos/home/home.component';
-
-// Login y Registro de usuario
-import { LoginComponent } from './modules/auth/login/login.component';
-import { RegistroComponent } from './modules/auth/registro/registro.component';
-import { LoginGuard } from './guard/login.guard';
-
-import { PedidosComponent } from './modules/pedidos/pedidos/pedidos.component';
-import { AppGuardService } from './guard/app-guard.service';
-import { ArticuloDetalleComponent } from './modules/articulos/articulo-detalle/articulo-detalle.component';
-
 const routes: Routes = [
-  {path: '', component: HomeComponent},
-  {path: 'login', component: LoginComponent, canActivate: [LoginGuard]},
-  {path: 'registro', component: RegistroComponent, canActivate: [LoginGuard]},
-  {path: 'pedidos', component: PedidosComponent, canActivate: [AppGuardService], data: { expectedRol: ['admin', 'user']}},
-  {path: 'detalle/:id', component: ArticuloDetalleComponent},
-  {path: '**', redirectTo:'', pathMatch: 'full'}
+  { path: 'home', loadChildren: () => import('./modules/articulos/articulos.module').then(m => m.ArticulosModule ) },
+  { path: 'auth', loadChildren: () => import('./modules/auth/auth.module').then(m => m.AuthModule) },
+  { path: 'pedidos', loadChildren: () => import('./modules/pedidos/pedidos.module').then(m => m.PedidosModule) },
+  { path: 'checkout', loadChildren: () => import('./modules/checkout/checkout.module').then(m => m.CheckoutModule) }, 
+  { path: '**', redirectTo:'home', pathMatch: 'full'}
 ];
 
 @NgModule({
