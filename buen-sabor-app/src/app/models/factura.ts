aac7606004e79997a1cf426feca14f59ba68f2a8
Bruno Tarditi <brunotarditi@hotmail.com>
2021-10-11 23:49:58 -0300

    Solucionando conflictos por merge en main

--- buen-sabor-app/src/app/models/factura.ts
+++ buen-sabor-app/src/app/models/factura.ts
@@ -4,8 +4,5 @@ export interface Factura {
     numeroFactura: number;
     fecha: Date;
     montoDescuento: number;
-    formaPago: string;
-    totalVenta: number;
-    totalCosto: number;
     pedido: Pedido;
 }
